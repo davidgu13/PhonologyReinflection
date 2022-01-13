ba51a62f94f9a8e2bd7e547fd134c6ee2d1d80cd
davidgu13 <davidgu1312@gmail.com>
2022-01-13 21:02:27 +0200

    Arranged the code + added arguments

--- generate_analogy_datasets.py
+++ generate_analogy_datasets.py
@@ -4,11 +4,8 @@ import random
 from data2samples_converter import Data2SamplesConverter
 from copy import deepcopy
 from tqdm import tqdm
-# from hyper_params_config import training_mode # , lang, POS, analogy_type
 import codecs
-
-lang, POS = 'kat', 'V'
-analogy_type = 'src_cross1'
+from argparse import ArgumentParser
 random.seed(1)
 
 # for debugging purposes:
@@ -26,13 +23,10 @@ def filter_dict_by_features(lemmas:dict, features:[str]) -> dict:
     return {lemma_name:feature_form_entries for lemma_name, feature_form_entries in lemmas.items()
             if has_features(feature_form_entries, features)}
 
-
 def join_fo(w:str): return w.replace(',','')
 def join_fe(w:str): return w.replace(',',';')
-
 def spl_fe(w:str): return w.split(';')
 def spl_fo(w:str): return list(w)
-
 
 def read_inflection_tables(inflection_file):
     """ read a standard inflection file (of the format "lemma\tform\tfeature") and construct a standard dictionary:
@@ -86,7 +80,6 @@ def track_kat_V_lemma(features_occurences, inflections_tuples, src_feat, src_for
         # if the source is not Masdar, we know exactly its lemma
         lemma = features_occurences[src_feat][src_form] # == features_dictionary[trg_feat][trg_form]
     return lemma
-
 
 def group_reinflection_samples_by_lemmas(features_occurences, inflections_tuples, reinflection_file):
     """
@@ -144,11 +137,11 @@ class AnalogyFunctionality(Data2SamplesConverter):
         self.features_dictionary, self.inflections_tuples = inflection_paradigms2list_and_dict(inflections_file)
         self.train_lemmas_dictionary = group_reinflection_samples_by_lemmas(self.features_dictionary, self.inflections_tuples, self.train_file)
 
-    def _generate_cross_1_datasets(self):
+    def generate_cross_1_datasets(self):
         """
         Either in form-split or lemma-split mode, the chosen auxiliary lemmas are taken from the train set. The possible lemmas are chosen by the current features.
         The goal is to filter the train lemmas according to the specific src & trg features (that are taken from the dev/test sets -- but it doesn't matter).
-        :return: 3 lists of reinflection samples
+        :return: 3 lists of src1-cross1 reinflection samples
         """
         train_samples = deepcopy(self.train_samples)
         dev_samples = deepcopy(self.dev_samples)
@@ -188,14 +181,10 @@ class AnalogyFunctionality(Data2SamplesConverter):
                 aux_src_form = possible_lemmas_dict[chosen_lemma][src_feat]
                 aux_trg_form = possible_lemmas_dict[chosen_lemma][trg_feat]
                 # Arrange all the elements into the new sample according to an agreed schema.
-                new_sample = ((src_feat, src_form, trg_feat, aux_src_form, aux_trg_form), trg_form)
+                new_sample = ((src_feat, src_form, aux_src_form, aux_trg_form, trg_feat), trg_form)
 
                 new_samples[-1].append(new_sample)
         return new_samples
-
-
-    def generate_data(self):
-        return self._generate_cross_1_datasets()
 
     # region static writing methods
     @staticmethod
@@ -262,60 +251,65 @@ def main():
     analogies_dir = join(original_dir, analogy_type)
     inflections_file = join(".data", "InflectionTables", f"{lang}.{POS}.txt")
     if not isdir(analogies_dir): mkdir(analogies_dir)
+
     print(f"Generating {analogy_type}-analogy datasets for {lang}.{POS}:")
     for training_mode in ['form', 'lemma']:
-        # original_files = [f"{training_mode}sp.schema2.{e}.txt" for e in ['train', 'dev', 'test']]
         original_files = [join(original_dir, f"{lang}.{POS}.{training_mode}.{e}.txt") for e in ['train', 'dev', 'test']]
 
         kat_analogies = AnalogyFunctionality(analogies_dir, inflections_file, *original_files)
-        analogised_train_dev_test_data = kat_analogies.generate_data() # analogised_train_dev_test_data is a [train, dev, test] list of datasets
-
+        analogised_train_dev_test_data = kat_analogies.generate_cross_1_datasets() # analogised_train_dev_test_data is a [train, dev, test] list of datasets
         print(f"Generating {training_mode}-split {analogy_type} analogy datasets:")
 
         for original_file, new_analogies_data in zip(original_files, analogised_train_dev_test_data): # iterates thrice - train, dev, test
             new_analogy_file = kat_analogies.analogy_reinflection2TSV(original_dir, basename(original_file), data=new_analogies_data)
             print(f"Generated data for {new_analogy_file}")
 
-def auxiliary_main():
-    # A short script for making sure the chosen auxiliary lemmas are differet from the original ones.
-    training_mode = 'form'
+def validate_kat_V_dataset(training_mode, subset):
+    # A short script for kat.V, that makes sure the chosen auxiliary lemmas are differet from the original ones.
     analogy_type = 'src1_cross1'
-    # original_dir = join(".data", "OriginalData")
-    analogies_dir = join(".data", "AnalogiesData", analogy_type)
-    inflections_file = join(".data", "OriginalData", "Inflection Tables Georgian.txt")
-    original_train_file, original_dev_file, original_test_file = [f"{training_mode}sp.schema2.{e}.txt" for e in ['train', 'dev', 'test']]
+    original_dir = join(".data", "Reinflection", f"{lang}.{POS}")
+    analogies_dir = join(original_dir, analogy_type)
+    inflections_file = join(".data", "InflectionTables", f"{lang}.{POS}.txt")
+    original_files = [join(original_dir, f"{lang}.{POS}.{training_mode}.{e}.txt") for e in ['train', 'dev', 'test']]
 
-    kat_analogies = AnalogyFunctionality(analogies_dir, inflections_file, original_train_file, original_dev_file, original_test_file)
+    kat_analogies = AnalogyFunctionality(analogies_dir, inflections_file, *original_files)
 
-    train_analog_path = join(analogies_dir, f"{training_mode}sp.schema2.train_{analogy_type}.tsv")
+    train_analog_path = join(analogies_dir, f"{lang}.{POS}.{training_mode}.{subset}.{analogy_type}.tsv")
     train_lines = open(train_analog_path, encoding='utf8').read().split('\n')
     train_lines = [line.strip().split('\t') for line in train_lines]
     train_data = []
     i=0
     for line in train_lines:
         src, trg = kat_analogies.analogy_sample2data(line)
-        if analogy_type=='src1_cross1':
-            fe1, fo1, fo_aux1, fo_aux2, fe2 = src
-            feature1_entry, feature2_entry = kat_analogies.features_dictionary[fe1], kat_analogies.features_dictionary[fe2]
-            bads = [] # 1st and 2nd error types stand for ambiguities at the lemmas correspond to a given Masdar forms.
-            if not feature1_entry[fo1] == feature2_entry[trg]:
-                bads.append((1, fo1, trg)) # Don't worry if it's printed
-            if not feature1_entry[fo_aux1] == feature2_entry[fo_aux2]:
-                bads.append((2, fo_aux1, fo_aux2)) # Don't worry if it's printed
-            if feature1_entry[fo1]==feature1_entry[fo_aux1]:
-                bads.append((3, fo1, fo_aux1)) # Do worry if it's printed
-            if bads:
-                print(f"{i+1}. The sample: {src, trg}. Bad ones: {bads}")
-                i+=1
+        src, trg = kat_analogies._cross1_sample2data(src, trg)
+        fe1, fo1, fo_aux1, fo_aux2, fe2 = src
+        feature1_entry, feature2_entry = kat_analogies.features_dictionary[fe1], kat_analogies.features_dictionary[fe2]
+        bads = [] # 1st and 2nd error types stand for ambiguities at the lemmas correspond to a given Masdar forms.
+        if not feature1_entry[fo1] == feature2_entry[trg]:
+            bads.append((1, fo1, trg)) # Don't worry if it's printed
+        if not feature1_entry[fo_aux1] == feature2_entry[fo_aux2]:
+            bads.append((2, fo_aux1, fo_aux2)) # Don't worry if it's printed
+        if feature1_entry[fo1]==feature1_entry[fo_aux1]:
+            bads.append((3, fo1, fo_aux1)) # Do worry if it's printed
+        if bads:
+            print(f"{i+1}. The sample: {src, trg}. Bad ones: {bads}")
+            i+=1
         train_data.append((src,trg))
     print("Done")
 
-# original_dir = join(".data", "OriginalData")
-# analogies_dir = join(".data", "AnalogiesData", analogy_type)
-# inflections_file = join(".data", "OriginalData", "Inflection Tables Georgian.txt")
-# original_train_file, original_dev_file, original_test_file = [f"{training_mode}sp.schema2.{e}.txt" for e in ['train', 'dev', 'test']]
-# analogies_converter = AnalogyFunctionality(original_dir, analogies_dir, inflections_file, original_train_file, original_dev_file, original_test_file)
-
 if __name__ == '__main__':
+    parser = ArgumentParser(description="Parse arguments for generating Analogy src1-cross1 datasets")
+    parser.add_argument('lang', type=str, choices=['bul', 'fin', 'hun', 'kat', 'lav', 'sqi', 'swc', 'tur'], help="Language to be processed")
+    parser.add_argument('POS', type=str, choices=['V','N','ADJ'], help="Part of speech to be processed")
+
+    args = parser.parse_args()
+    lang, POS = args.lang, args.POS
+    analogy_type = 'src1_cross1'
     main()
-    # auxiliary_main()
+
+    # region validate kat.V
+    if lang == 'kat' and POS == 'V':
+        for a in ['form', 'lemma']:
+            for b in ['train', 'dev', 'test']:
+                validate_kat_V_dataset(a, b)
+    # endregion validate kat.V
