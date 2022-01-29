d975dc5a48ccc8a25ec44a9fffa06c235fbecfec
davidgu13 <davidgu1312@gmail.com>
2022-01-29 13:02:46 +0200

    fixed argv bug; minor updates

--- test_all_models.py
+++ test_all_models.py
@@ -1,6 +1,5 @@
 from os.path import join, isdir, isfile
-from os import mkdir, listdir, getcwd
-from test_single_model import test_single_model
+from os import mkdir, listdir
 from argparse import ArgumentParser
 
 original_main_logs_folder, test_main_logs_folder = join("Results", "Logs"), join("Results", "Logs", "Tests")
@@ -17,14 +16,19 @@ def main(analogy_type, seed, device_idx):
     2. Divide the work to the various GPUs/servers.
     3. Apply extract_results..py with the new Excel path.
     """
+    from test_single_model import test_single_model
+
     analogy_seed_folder = f"{analogy_type}_{seed}"
+    groups_indices = [3]
     for i in groups_indices: # 7 groups
         original_logs_folder = join(original_main_logs_folder, analogy_seed_folder, f"Group {i}")
         test_logs_folder = join(test_main_logs_folder, analogy_seed_folder, f"Group {i}")
         models_folder = join("Results", "Checkpoints", analogy_seed_folder, f"Group {i}")
         if not isdir(test_logs_folder): mkdir(test_logs_folder)
 
-        log_files = [f for f in listdir(original_logs_folder) if isfile(f)] # can be 6/12/18 files.
+        log_files = [f for f in listdir(original_logs_folder) if isfile(join(original_logs_folder, f))] # can be 6/12/18 files.
+        print(f"original_logs_folder = {original_logs_folder}")
+        print(f"Looping through the {len(log_files)} files in {log_files}.")
         for f in log_files:
             # Replacing from:    Logs_2022-01-15 003659_bul_ADJ_form_f_p_src1_cross1_42_0_attn.txt
             # to: Model_Checkpoint_46_2022-01-15 003659_bul_ADJ_form_f_p_src1_cross1_42_0_attn.pth.tar
@@ -34,10 +38,12 @@ def main(analogy_type, seed, device_idx):
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
+    parser = ArgumentParser(description="Test the models given some parameters")
     parser.add_argument('analogy_type', type=str, choices=['src1_cross1', 'None'], help='The analogies type to be applied')
     parser.add_argument('SEED', type=int, help='Initial seed for all random operations')
     parser.add_argument('device_idx', type=str, help='GPU index', nargs='?', default='0')
     args = parser.parse_args()
     analogy_type, seed, device_idx = args.analogy_type, args.SEED, args.device_idx
+    import sys
+    sys.argv = [sys.argv[0]]
     main(analogy_type, seed, device_idx)
