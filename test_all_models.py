2ebb81cb8697b997c8b684447e4f589b41549080
davidgu13 <davidgu1312@gmail.com>
2022-01-29 15:14:12 +0200

    printing to bash file calls of test_all_models.py

--- test_all_models.py
+++ test_all_models.py
@@ -19,7 +19,7 @@ def main(analogy_type, seed, device_idx):
     from test_single_model import test_single_model
 
     analogy_seed_folder = f"{analogy_type}_{seed}"
-    groups_indices = [3]
+    # groups_indices = [3] # for debugging purposes
     for i in groups_indices: # 7 groups
         original_logs_folder = join(original_main_logs_folder, analogy_seed_folder, f"Group {i}")
         test_logs_folder = join(test_main_logs_folder, analogy_seed_folder, f"Group {i}")
@@ -27,15 +27,15 @@ def main(analogy_type, seed, device_idx):
         if not isdir(test_logs_folder): mkdir(test_logs_folder)
 
         log_files = [f for f in listdir(original_logs_folder) if isfile(join(original_logs_folder, f))] # can be 6/12/18 files.
-        print(f"original_logs_folder = {original_logs_folder}")
-        print(f"Looping through the {len(log_files)} files in {log_files}.")
+        # print(f"original_logs_folder = {original_logs_folder}")
+        # print(f"Looping through the {len(log_files)} files in {log_files}.")
         for f in log_files:
             # Replacing from:    Logs_2022-01-15 003659_bul_ADJ_form_f_p_src1_cross1_42_0_attn.txt
             # to: Model_Checkpoint_46_2022-01-15 003659_bul_ADJ_form_f_p_src1_cross1_42_0_attn.pth.tar
             pattern = f.replace("Logs_","").replace(".txt","")
             model_file = [f for f in listdir(models_folder) if pattern in f][0]
-            test_single_model(model_file, device_idx, test_logs_folder, models_folder)
-
+            # test_single_model(model_file, test_logs_folder, models_folder, device_idx)
+            open(commands_file, "a+").write(f'python test_single_model.py "{model_file}" "{test_logs_folder}" "{models_folder}" {device_idx}\n')
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Test the models given some parameters")
@@ -44,6 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('device_idx', type=str, help='GPU index', nargs='?', default='0')
     args = parser.parse_args()
     analogy_type, seed, device_idx = args.analogy_type, args.SEED, args.device_idx
+    commands_file = f"Test runs-{analogy_type}-{seed}-{device_idx}.sh"
     import sys
     sys.argv = [sys.argv[0]]
     main(analogy_type, seed, device_idx)
